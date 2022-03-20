d46a7d0870ba1a22822c4fc80484aa4028e5d312
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-03-21 01:44:20 +0300

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
         <nav className="container flex justify-center items-center py-8 mx-auto bg-transparent">
-            <div>
-                <a href="/"><h3 className="text-3xl font-bold text-[#5ff59b]">Filmtika</h3></a>
-            </div>
+            <Link href="/">
+                <a><h3 className="text-3xl font-bold text-[#5ff59b]">Filmtika</h3></a>
+            </Link>
         </nav>
     )
 }
